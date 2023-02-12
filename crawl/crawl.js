1473b5797caacd73ad42b9a5c52d0904e53035cb
ibrahimgb <ibrahim.guoual.b@gmail.com>
2023-02-12 20:24:00 +0100

    create web crawler  on a single page

--- crawl/crawl.js
+++ crawl/crawl.js
@@ -1,5 +1,27 @@
 const {JSDOM} = require('jsdom')
 
+async function crawlPage(currentURL){
+    console.log(`currently crawling: ${currentURL}`)
+    try{
+        const resp = await fetch(currentURL)
+        if (resp.status > 399){
+            console.log(`errer in fetch with status ${resp.status} on page ${currentURL}`)
+            return
+        }
+
+        const contentType = resp.headers.get("content-type")
+
+        if(!contentType.includes('text/html')){
+            console.log(`errer no HTML responce, content type: ${contentType}, on page ${currentURL}`)
+            return
+        }
+
+
+        console.log( await resp.text())
+    }catch(err){
+        console.log(`error in fetch: ${err.message}, on page ${currentURL}`)
+    }
+}
 function getURLFromHTML(htmlBody, baseURL){
     const urls =  []
     const dom = new JSDOM(htmlBody)
@@ -43,5 +65,6 @@ function normalizeURL(urlString){
 
 module.exports = {
     normalizeURL,
-    getURLFromHTML
+    getURLFromHTML,
+    crawlPage
 }
